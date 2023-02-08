7934315a1b86b4b84a633761e6a842121e9c174f
LeHaine <colt.daily@gmail.com>
2023-02-08 14:19:54 -0500

    upgrade to littlekt 0.6.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,9 @@ allprojects {
     val isReleaseVersion = !runeVersion.endsWith("SNAPSHOT")
     extra["isReleaseVersion"] = isReleaseVersion
 }
+
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+        yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.WARNING
+    }
+}
